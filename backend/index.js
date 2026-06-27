c3b7daf23ac84f3c7d4bf2ac358a1a1acf104194
BellaLiker <phanna069@gmail.com>
2026-06-27 12:40:34 +0700

    update-ecom

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const db = require("./db");
+const client = require("prom-client");
 
 const app = express();
 
+// Collect default Node.js metrics
+client.collectDefaultMetrics();
+
+// Create a Registry
+const register = new client.Registry();
+register.setDefaultLabels({
+  app: "ecom-backend",
+});
+
+client.collectDefaultMetrics({ register });
+
+// Health Check
 app.get("/health", (req, res) => {
   res.json({
     status: "healthy",
   });
 });
 
+// Products API
 app.get("/products", (req, res) => {
   db.query("SELECT * FROM products", (err, results) => {
     if (err) {
@@ -17,6 +31,12 @@ app.get("/products", (req, res) => {
 
     res.json(results);
   });
+});
+
+// Prometheus Metrics
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", register.contentType);
+  res.end(await register.metrics());
 });
 
 app.listen(5000, () => {
