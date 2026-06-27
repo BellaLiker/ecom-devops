9e55b73f2cc0ba4ab5fa499bdb824e81a4624534
BellaLiker <phanna069@gmail.com>
2026-06-27 22:22:41 +0700

    add monitoring stack - alertmanager, loki, node-exporter, cadvisor

--- backend/index.js
+++ backend/index.js
@@ -4,16 +4,49 @@ const client = require("prom-client");
 
 const app = express();
 
-// Collect default Node.js metrics
-client.collectDefaultMetrics();
-
-// Create a Registry
+// Create a registry
 const register = new client.Registry();
-register.setDefaultLabels({
-  app: "ecom-backend",
-});
 
+// Collect default Node.js metrics
 client.collectDefaultMetrics({ register });
+
+// Custom Counter
+const httpRequests = new client.Counter({
+  name: "ecom_http_requests_total",
+  help: "Total HTTP requests",
+  registers: [register],
+});
+const requestDuration = new client.Histogram({
+  name: "ecom_http_request_duration_seconds",
+  help: "Duration of HTTP requests in seconds",
+  labelNames: ["method", "route", "status_code"],
+  buckets: [0.05, 0.1, 0.2, 0.5, 1, 2, 5],
+  registers: [register],
+});
+const dbQueryDuration = new client.Histogram({
+  name: "ecom_mysql_query_duration_seconds",
+  help: "Time spent executing MySQL queries",
+  labelNames: ["query"],
+  buckets: [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1],
+  registers: [register],
+});
+// Count every request
+
+app.use((req, res, next) => {
+  httpRequests.inc();
+
+  const end = requestDuration.startTimer();
+
+  res.on("finish", () => {
+    end({
+      method: req.method,
+      route: req.path,
+      status_code: res.statusCode,
+    });
+  });
+
+  next();
+});
 
 // Health Check
 app.get("/health", (req, res) => {
@@ -24,16 +57,25 @@ app.get("/health", (req, res) => {
 
 // Products API
 app.get("/products", (req, res) => {
+
+  const end = dbQueryDuration.startTimer();
+
   db.query("SELECT * FROM products", (err, results) => {
+
+    end({
+      query: "select_products",
+    });
+
     if (err) {
       return res.status(500).json(err);
     }
 
     res.json(results);
   });
+
 });
 
-// Prometheus Metrics
+// Metrics
 app.get("/metrics", async (req, res) => {
   res.set("Content-Type", register.contentType);
   res.end(await register.metrics());
